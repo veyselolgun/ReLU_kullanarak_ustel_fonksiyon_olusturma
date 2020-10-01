0da6955fd2510db35ab2eff0257f6d1b62642df0
veysel <49754308+veyselolgun@users.noreply.github.com>
2020-10-01 20:10:04 +0300

    Add files via upload
    Optimizasyon gerçekleştirildi

---
+++ optimized_exp_by_ReLU.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+def draw_graph(step,color):
+    x,y = [],[]
+    sum = 0
+
+    for i in frange(0,10,step):
+        x.append(i)
+
+    for i,j in enumerate(frange(0,10,step)):
+        sum=sum+max(0,x[i])+x[i]
+        y.append(sum)
+        
+    print(x)
+    print(y)
+
+    plt.plot(x, y, color, linewidth=2)
+    plt.title('Step Size = {}'.format(step))
+
+
+
+draw_graph(1,"red")
+plt.show()
