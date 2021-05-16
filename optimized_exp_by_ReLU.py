69999e02697e422a34dd2ae0f6cba21eda60c54f
veysel <49754308+veyselolgun@users.noreply.github.com>
2021-05-16 23:45:49 +0300

    Update optimized_exp_by_ReLU.py

--- optimized_exp_by_ReLU.py
+++ optimized_exp_by_ReLU.py
@@ -6,20 +6,14 @@ def frange(start, stop, step):
         yield i
         i += step
 
-def draw_graph(step,color):
+def draw_exp_graph(step,color):
     x,y = [],[]
     sum = 0
-
-    #for i in frange(0,10,step):
-    #    x.append(i)
 
     for i,j in enumerate(frange(0,10,step)):
         x.append(i)
         sum=sum+max(0,x[i])
         y.append(sum)
-        
-    print(x)
-    print(y)
 
     plt.plot(x, y, color, linewidth=2)
     plt.title('Step Size = {}'.format(step))
